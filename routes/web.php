b1aa4b6f21dc9646ad2d33c46eaef8ce93041c09
graydon-whitespark <graydon@whitespark.ca>
2025-04-09 19:21:06 -0600

    refactor web.php to leverage inertia shorthand routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::inertia('/', 'Welcome')->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
-});
+Route::inertia('dashboard', 'Dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
